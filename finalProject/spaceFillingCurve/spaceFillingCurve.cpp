1a547a2e7eefc0fb1c0043998dc46f3874aca00a
Marcel Rodriguez-Riccelli <marcelrodriguezricc@gmail.com>
2024-03-10 16:48:19 -0700

    continued work on space filling curve

--- finalProject/spaceFillingCurve/spaceFillingCurve.cpp
+++ finalProject/spaceFillingCurve/spaceFillingCurve.cpp
@@ -1,8 +1,7 @@
 // Space Filling Curve:
 // 1. Start with a spherical mesh.
 // - Apply golden ratio to evenly distribute points on the sphere's surface.
-// - Draw lines based on three nearest neighbors for each point to create the mesh.
-
+//
 // 2. Apply dual of simplest subdivision. Label these new edges "new."
 // - Find four nearest neighbors to each vertex.
 //
@@ -17,6 +16,7 @@
 // AlloLib Libraries:
 #include "al/app/al_App.hpp" // App library.
 #include "al/graphics/al_Shapes.hpp" // Shapes library.
+#include <queue> // Priority queue library.
 
 // Namespaces:
 using namespace al; // "al" namespace.
@@ -25,44 +25,70 @@ using namespace std;// "std" namespace.
 #define PI 3.14159265
 #define TWO_PI 3.14159265 * 2
 
-const int numVertices = 500;
-Vec3f sphereVertices[numVertices];
+// Data structure to store nearest neighbors:
+struct nn {
+  Vec3f n1, n2, n3, n4;
+};
+
+// Custom comparator for the priority queue:
+struct CompareDist {
+    bool operator()(pair<Vec3f, float> const& p1, pair<Vec3f, float> const& p2) { 
+        return p1.second > p2.second; // Returns a boolean (true / false) based on distance from p1 being greater than than distance from p2.
+    }
+};
 
 struct MyApp : public App {
-  Mesh oldMesh, newMesh, curveMesh; // The original mesh, the subdivided mesh, and the final curve to be displayed.
+  Mesh ogMesh, facePoints, edgePoints; // The original mesh, the subdivided mesh, and the final curve to be displayed.
+  priority_queue<pair<Vec3f, float>, vector<pair<Vec3f, float>>, CompareDist> pq; // Create a priority queue that holds pairs of Vec3f vertices and the float distance between them, ordered by the custom comparator.
+  Vec3f vertex1, vertex2, vertex3, centroid;
 
   void onCreate() override {
-    oldMesh.primitive(Mesh::LINES);
     nav().pos(0, 0, 0);
 
     // 1. Create a Spherical Mesh:
-    // Apply golden ratio to evenly distribute points on the sphere's surface:
-    for(int i = 0; i < numVertices; i++){
-      float theta = acos(1 - 2 * float(i) / numVertices); // Calculate angles based on point number to be equally distributed along sphere's surface.
-      float phi = PI * (1 + sqrt(5)) * float(i); // The golden ratio.
-      sphereVertices[i] = Vec3f(cos(phi) * sin(theta), sin(phi) * sin(theta), cos(theta)); // Calculate the point on the sphere's surface.
+
+    addIcosphere(ogMesh, 1, 2);
+    for (int i = 0; i < ogMesh.vertices().size(); i++){
+      ogMesh.color(HSV(1.0, 1.0, 1.0));
     }
 
-    // Draw lines based on three nearest neighbors for each point to create the mesh:
-    for (int i = 0; i < numVertices; i++){
-      oldMesh.vertex(sphereVertices[i]);
-      oldMesh.color(HSV(float(i) / numVertices, 1.0, 1.0));
-      float dist = 0.0; // Initialize the distance.
-      float nearest1 = 5.0; // Initialize the nearest neighbor.
-      Vec3f p1, p2, p3;
-      for (int j = i + 1; j < numVertices; j++){
-        dist = (sphereVertices[i] - sphereVertices[j]).mag();
-        if (dist < nearest1) {
-          nearest1 = dist;
-          p1 = sphereVertices[j];
-        }
+    facePoints.primitive(Mesh::POINTS);
+    ogMesh.forEachFace([&](int v1, int v2, int v3) {
+      vertex1 = ogMesh.vertices()[v1];
+      vertex2 = ogMesh.vertices()[v2];
+      vertex3 = ogMesh.vertices()[v3];
+      centroid = (vertex1 + vertex2 + vertex3) / 3.0f;
+      facePoints.color(HSV(1.0, 0.0, 1.0));
+      facePoints.vertex(centroid);
+    });
+
+    nn nearestFP[facePoints.vertices().size()]; // Array of nearest neighbors for each facepoint.
+
+    // Find nearest neighbors:
+    for (int i = 0; i < facePoints.vertices().size(); i++) {
+      while (!pq.empty()) { // If the priority queue is not empty...
+        pq.pop(); // Pop the top element until it's empty.
       }
-      oldMesh.vertex(p1);
+      for (int j = 0; j < facePoints.vertices().size(); j++){
+        if (i != j){ // Do not calculate for self.
+          float dist = (facePoints.vertices()[i] - facePoints.vertices()[j]).mag(); // Calculate the distance between two vertices.
+          pq.push(make_pair(facePoints.vertices()[j], dist)); // Push a pair of vertices and their distance to the priority queue.
+          }
+        }
+      // Assign the nearest vertices in priority queue to the nearestNeighbors array:
+      nearestFP[i].n1 = pq.top().first; // Assign the nearest vertex to the nearestNeighbors array.
+      pq.pop(); // Pop the top element from the priority queue so now the second nearest vertex is at the top.
+      nearestFP[i].n2 = pq.top().first; // Assign the second nearest vertex to n2, and so on...
+      pq.pop();
     }
 
-    // 2. Apply simplest subdivision to obtain new mesh:
-    // Find four nearest neighbors to each vertex:
-    int numVertices = oldMesh.vertices().size();
+    // Set edge points to the average of two neighboring face points:
+    edgePoints.primitive(Mesh::POINTS);
+    for (int i = 0; i < facePoints.vertices().size(); i++) {
+      edgePoints.color(HSV(0.3, 1.0, 1.0));
+      Vec3f avg = (facePoints.vertices()[i] + nearestFP[i].n1) / 2.0f;
+      edgePoints.vertex(avg);
+    }
   }
 
   void onAnimate(double dt) override{
@@ -83,7 +109,11 @@ struct MyApp : public App {
     // Draw mesh to scene:
     g.polygonMode(true ? GL_LINE : GL_FILL); // Make the mesh wireframe.
     g.meshColor();
-    g.draw(oldMesh);
+    g.pointSize(5.0);
+    g.lineWidth(5.0);
+    g.draw(ogMesh);
+    g.draw(facePoints);
+    g.draw(edgePoints);
   }
 };
 
